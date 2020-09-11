613593eb43621bf2fa5f28e86bd1640024bc4961
kwj2435 <dmlwls2435@naver.com>
2020-09-11 11:22:00 +0900

    first push

---
+++ src/main/java/web/com/board/HierarchicalBoardApplication.java
@@ -0,0 +1,13 @@
+package web.com.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HierarchicalBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HierarchicalBoardApplication.class, args);
+	}
+
+}
